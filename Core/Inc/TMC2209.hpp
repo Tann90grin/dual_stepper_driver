9620ef6f1457f253f4f994de45a8eeac44530c6b
Henry Wu <h27wu@uwaterloo.ca>
2025-03-23 21:27:04 -0400

    setup class

---
+++ Core/Inc/TMC2209.hpp
@@ -0,0 +1,18 @@
+/*
+ * TMC2209.hpp
+ *
+ *  Created on: Mar 23, 2025
+ *      Author: Henry
+ */
+
+#ifndef INC_TMC2209_HPP_
+#define INC_TMC2209_HPP_
+
+class TMC2209{
+	private:
+
+	public:
+
+};
+
+#endif /* INC_TMC2209_HPP_ */
---
+++ Core/Src/TMC2209.cpp
@@ -0,0 +1,10 @@
+/*
+ * TMC2209.cpp
+ *
+ *  Created on: Mar 23, 2025
+ *      Author: henry
+ */
+
+
+
+
